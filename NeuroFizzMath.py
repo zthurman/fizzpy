31e9152d55121214b8a70ef2047b78f381a724be
wonkybadank <zthurman@users.noreply.github.com>
2014-09-14 13:46:20 -0700

    Update NeuroFizzMath.py

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -19,8 +19,20 @@ import math as mt
 # 	 together to come up with the solution
 
 class Neuron:
-	def RK4():
-	
+	def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):  
+    		tsp = np.arange(t0, t1, dt)
+    		Nsize = np.size(tsp)
+    		X = np.empty((Nsize, np.size(x0)))
+    		X[0] = x0
+
+    		for i in range(1, Nsize):
+        		k1 = ng(X[i-1],tsp[i-1])
+        		k2 = ng(X[i-1] + dt/2*k1, tsp[i-1] + dt/2)
+        		k3 = ng(X[i-1] + dt/2*k2, tsp[i-1] + dt/2)
+		        k4 = ng(X[i-1] + dt*k3, tsp[i-1] + dt)
+        		X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
+    		      return X
+
 	def FN(x,t,a = 0.75,b = 0.8,c = 3, I = -0.40):
     		return np.array([c*(x[0]+ x[1]- x[0]**3/3 + I), \
                     	-1/c*(x[0]- a + b*x[1])])
