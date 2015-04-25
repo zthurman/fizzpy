d40262375abd5ef70d7b3bff467f4831cd82230b
zthurman <zrt81491@gmail.com>
2015-04-25 15:31:31 -0700

    cleaned everything up, warm fuzzies

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -14,6 +14,9 @@ class Neuron():
     def __init__(self, name):
         self.name = name
 
+    def model(self,x,t):
+        pass
+
     def rk4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
         tsp = np.arange(t0, t1, dt)
         Nsize = np.size(tsp)
@@ -40,13 +43,14 @@ class ML(Neuron):
     name = 'Morris-Lecar'
     x0 = np.array([0,0])
 
-    def model(v,t,c = 20,vk=-84,gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,i = 79):
-        return np.array([(-gca*(0.5*(1 + mt.tanh((v[0] - v1)/v2)))*(v[0]-vca) - gk*v[1]*(v[0]-vk) - gl*(v[0]-vl) + i),
-                        (phi*((0.5*(1 + mt.tanh((v[0] - v3)/v4))) - v[1]))/(1/mt.cosh((v[0] - v3)/(2*v4)))])
+    def model(x,t,c = 20,vk=-84,gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,i = 79):
+        return np.array([(-gca*(0.5*(1 + mt.tanh((x[0] - v1)/v2)))*(x[0]-vca) - gk*x[1]*(x[0]-vk) - gl*(x[0]-vl) + i),
+                        (phi*((0.5*(1 + mt.tanh((x[0] - v3)/v4))) - x[1]))/(1/mt.cosh((x[0] - v3)/(2*v4)))])
 
 
 class IZ(Neuron):
     name = 'Izhikevich'
+    x0 = np.array([0,0])
 
     def model(x,t, a = 0.02, b = 0.2, c = -65, d = 2, i = 10):
         if x[0] >= 30:
@@ -57,30 +61,32 @@ class IZ(Neuron):
 
 
 class HR(Neuron):
-	self.params = {}
-	self.inputs = 
-	def HR2(x,t, a = 1.0, b = 3.0, c = 1.0, d = 5.0, r = 0.006, s = 4.0, I = 1.84, xnot = -1.5, k = 0.05):
-	    return np.array([x[1] - a*(x[0]**3) + (b*(x[0]**2)) - x[2] + I + k*(x[3] - x[0]), \
-		            c - d*(x[0]**2) - x[1], \
-		            r*(s*(x[0] - xnot) - x[2]), \
-		            x[4] - a*(x[3]**3) + (b*(x[3]**2)) - x[5] + I + k*(x[0] - x[3]), \
-		            c - d*(x[3]**2) - x[4], \
-		            r*(s*(x[3] - xnot) - x[5])])
+    name = 'Hindmarsh-Rose'
+    x0 = np.array([3, 0, -1.2])
+
+    def model(x,t, a = 1.0, b = 3.0, c = 1.0, d = 5.0, r = 0.006, s = 4.0, I = 1.84, xnot = -1.5, k = 0.05):
+        return np.array([x[1] - a*(x[0]**3) + (b*(x[0]**2)) - x[2] + I + k*(x[3] - x[0]),
+                        c - d*(x[0]**2) - x[1],
+                        r*(s*(x[0] - xnot) - x[2]),
+                        x[4] - a*(x[3]**3) + (b*(x[3]**2)) - x[5] + I + k*(x[0] - x[3]),
+                        c - d*(x[3]**2) - x[4],
+                        r*(s*(x[3] - xnot) - x[5])])
 
 class HH(Neuron):
-	self.params = {}
-	self.inputs = 
-	def HH(x,t, g_K=36, g_Na=120, g_L=0.3, E_K=12, E_Na=-115, E_L=-10.613, C_m=1, I=-10):
-	    alpha_n = (0.01*(x[0]+10))/(exp((x[0]+10)/10)-1)
-	    beta_n = 0.125*exp(x[0]/80)
-	    alpha_m = (0.1*(x[0]+25))/(exp((x[0]+25)/10)-1)
-	    beta_m = 4*exp(x[0]/18)
-	    alpha_h = (0.07*exp(x[0]/20))
-	    beta_h = 1 / (exp((x[0]+30)/10)+1)
-	    return np.array([(g_K*(x[1]**4)*(x[0]-E_K) + g_Na*(x[2]**3)*x[3]*(x[0]-E_Na) + g_L*(x[0]-E_L) - I)*(-1/C_m), \
-		              alpha_n*(1-x[1]) - beta_n*x[1], \
-		              alpha_m*(1-x[2]) - beta_m*x[2], \
-		              alpha_h*(1-x[3]) - beta_h*x[3]])
+    name = 'Hodgkins-Huxley'
+    x0 = np.array([0.01,0.01,0.01,0.01])
+
+    def model(x,t, g_K=36, g_Na=120, g_L=0.3, E_K=12, E_Na=-115, E_L=-10.613, C_m=1, I=-10):
+        alpha_n = (0.01*(x[0]+10))/(exp((x[0]+10)/10)-1)
+        beta_n = 0.125*exp(x[0]/80)
+        alpha_m = (0.1*(x[0]+25))/(exp((x[0]+25)/10)-1)
+        beta_m = 4*exp(x[0]/18)
+        alpha_h = (0.07*exp(x[0]/20))
+        beta_h = 1 / (exp((x[0]+30)/10)+1)
+        return np.array([(g_K*(x[1]**4)*(x[0]-E_K) + g_Na*(x[2]**3)*x[3]*(x[0]-E_Na) + g_L*(x[0]-E_L) - I)*(-1/C_m),
+                        alpha_n*(1-x[1]) - beta_n*x[1],
+                        alpha_m*(1-x[2]) - beta_m*x[2],
+                        alpha_h*(1-x[3]) - beta_h*x[3]])
 	
 	
 		
