6332eed30d520ea50b09c0ec07acf53923561cfb
zthurman <zrt81491@gmail.com>
2015-04-25 14:14:27 -0700

    first pycharm commit, waaaaaaaaaaaay more fancy than gedit

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -2,7 +2,6 @@
 #  Classes for different neuron models that all inherit from parent neuron.
 
 from __future__ import division
-from Neuro import *
 from scipy import *
 import numpy as np
 import pylab
@@ -12,22 +11,22 @@ import sys
 import math as mt
 
 class Neuron():
-	def __init__(self, params, inputs, model):
-	self.params = []
-	self.inputs = []
+    def __init__(self, params, inputs, model):
+        self.params = []
+        self.inputs = []
 	
-	def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):  
-	    tsp = np.arange(t0, t1, dt)
-	    Nsize = np.size(tsp)
-	    X = np.empty((Nsize, np.size(x0)))
-	    X[0] = x0
-	    for i in range(1, Nsize):
-		k1 = ng(X[i-1],tsp[i-1])
-		k2 = ng(X[i-1] + dt/2*k1, tsp[i-1] + dt/2)
-		k3 = ng(X[i-1] + dt/2*k2, tsp[i-1] + dt/2)
-		k4 = ng(X[i-1] + dt*k3, tsp[i-1] + dt)
-		X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
-	    return X
+    def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
+        tsp = np.arange(t0, t1, dt)
+        Nsize = np.size(tsp)
+        X = np.empty((Nsize, np.size(x0)))
+        X[0] = x0
+        for i in range(1, Nsize):
+        k1 = ng(X[i-1],tsp[i-1])
+        k2 = ng(X[i-1] + dt/2*k1, tsp[i-1] + dt/2)
+        k3 = ng(X[i-1] + dt/2*k2, tsp[i-1] + dt/2)
+        k4 = ng(X[i-1] + dt*k3, tsp[i-1] + dt)
+        X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
+        return X
 
 class FN(Neuron):
 	self.params = {a = 0.75, b = 0.8, c = 3}
