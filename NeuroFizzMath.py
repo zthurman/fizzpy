1b4b6d279146bf45111ac548fa703e311925ab8d
zthurman <zrt81491@gmail.com>
2015-04-25 15:06:45 -0700

    refactored Neuron class, still working on getting FN solid

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -11,11 +11,10 @@ import sys
 import math as mt
 
 class Neuron():
-    def __init__(self, params, inputs):
-        self.params = []
-        self.inputs = []
-	
-    def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
+    def __init__(self,name):
+        self.name = name
+
+    def rk4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
         tsp = np.arange(t0, t1, dt)
         Nsize = np.size(tsp)
         X = np.empty((Nsize, np.size(x0)))
@@ -29,17 +28,20 @@ class Neuron():
         return X
 
 class FN(Neuron):
-    params = {'a': 0.75, 'b': 0.8, 'c': 3}
-    inputs = -1.476
-    def model(x,t, params['a'], params['b'], params['c'], inputs):
-        return np.array([self.params['c']*(x[0]+ x[1]- x[0]**3/3 + self.inputs), \
-                        -1/self.params['c']*(x[0]- self.params['a'] + self.params['b']*x[1])])
-	  
+    def __init__(self, name, params, inputs, x, t):
+        self.name = 'Fitzhugh-Nagumo'
+        self.params = [0.75, 0.8, 3]        # a, b and c constants
+        self.i = [-1.476]            # input stimulus
+        self.x0 = np.array([0.01,0.01])
+
+    def model(x,t, params[0], params[1], params[2], i):
+        return np.array([params[2]*(x[0]+ x[1]- x[0]**3/3 + inputs), -1/params[2]*(x[0]- params[0] + params[1]*x[1])])
+
 
 class ML(Neuron):
 	self.params = {c = 20,vk = -84,gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30}
 	self.inputs = 79
-	def ML(v,t,self.params['c'],self.params['vk'],gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,self.inputs):
+	def model(v,t,self.params['c'],self.params['vk'],gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,self.inputs):
 	    return np.array([(-gca*(0.5*(1 + mt.tanh((v[0] - v1)/v2)))*(v[0]-vca) - gk*v[1]*(v[0]-vk) - gl*(v[0]-vl) + Iapp), \
 		             (phi*((0.5*(1 + mt.tanh((v[0] - v3)/v4))) - v[1]))/(1/mt.cosh((v[0] - v3)/(2*v4)))])
 
