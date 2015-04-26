3b82fdf0d8464c5581ba210e4ccabbdd1d70d66f
zthurman <zrt81491@gmail.com>
2015-04-26 11:38:10 -0700

    doing basic window stuff with PyQt

--- NeuroFizzMathUI.py
+++ NeuroFizzMathUI.py
@@ -2,4 +2,26 @@
 # User interface for program
 
 import NeuroFizzMath
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+import sys
+from PyQt4 import QtGui
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('NeuroFizzMath')
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
 
