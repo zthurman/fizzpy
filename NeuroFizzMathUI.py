ec4ce164f37feb0995c35029aed4a8e1e6decedf
zthurman <zrt81491@gmail.com>
2015-04-26 12:12:27 -0700

    I think I'm going to practice some and get a handle on what the hell is going on first

--- NeuroFizzMathUI.py
+++ NeuroFizzMathUI.py
@@ -9,48 +9,44 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-class Example(QtGui.QMainWindow):
-
+class Cheese(QtGui.QMainWindow):
+    
     def __init__(self):
-        super(Example, self).__init__()
-
+        super(Cheese, self).__init__()
+        
         self.initUI()
+        
+        
+    def initUI(self):               
+        
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
 
-
-    def initUI(self):
-
-        self.resize(250, 150)
-        self.center()
-
-        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        exitAction = QtGui.QAction(QtGui.QIcon('exit24.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(QtGui.qApp.quit)
+        exitAction.triggered.connect(self.close)
 
-        self.statusBar().showMessage('Ready')
+        self.statusBar()
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('NeuroFizzMath')
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+        
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')    
         self.show()
-
-
-    def center(self):
-
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
+        
+        
 def main():
-
+    
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = Cheese()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    main()
+    main()    
