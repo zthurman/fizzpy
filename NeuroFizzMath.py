672ff7342f9fdc33d09e34de251deff78ac569f0
zthurman <zrt81491@gmail.com>
2015-05-06 23:07:11 -0700

    housekeeping, modified rk4 algorithm such that no longer doing the subtractions at each step and re-initialized range of for loop to 0. Added room for Euler solver

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+# NeuroFizzMath
+# Copyright (C) 2015 Zechariah Thurman
+
 #  Classes for different differential eqn models. All the math
-# resides here
+# resides here.
 
 from __future__ import division
 from scipy import *
@@ -11,6 +14,11 @@ from matplotlib import pyplot as plt
 import sys
 import math as mt
 
+
+# global Euler solver
+
+
+
 # global Runge-Kutte solver
 
 def rk4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
@@ -18,12 +26,12 @@ def rk4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
     Nsize = np.size(tsp)
     X = np.empty((Nsize, np.size(x0)))
     X[0] = x0
-    for i in range(1, Nsize):
-        k1 = ng(X[i-1],tsp[i-1])
-        k2 = ng(X[i-1] + dt/2*k1, tsp[i-1] + dt/2)
-        k3 = ng(X[i-1] + dt/2*k2, tsp[i-1] + dt/2)
-        k4 = ng(X[i-1] + dt*k3, tsp[i-1] + dt)
-        X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
+    for i in range(0, Nsize-1):
+        k1 = ng(X[i],tsp[i-1])
+        k2 = ng(X[i] + dt/2*k1, tsp[i] + dt/2)
+        k3 = ng(X[i] + dt/2*k2, tsp[i] + dt/2)
+        k4 = ng(X[i] + dt*k3, tsp[i] + dt)
+        X[i+1] = X[i] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
     return X
 
 # System, super class of all models
