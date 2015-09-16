f2d2059bad84a6711e37cdb0a82df25c8c544096
wonkybadonk <zthurman@users.noreply.github.com>
2015-09-15 18:26:47 -0700

    changed copyright dialog

--- NeuroFizzMathUI.py
+++ NeuroFizzMathUI.py
@@ -1238,9 +1238,9 @@ class ApplicationWindow(QtGui.QMainWindow):
 
         This program is free software; you can redistribute
         it and/or modify it under the terms of the GNU
-        General Public License as published by the Free Software
-        Foundation; either version 2 of the License, or (at
-        your option) any later version.
+        General Public License as published by the Free
+        Software Foundation; either version 2 of the License,
+        or (at your option) any later version.
 
         This program is distributed in the hope that it will be
         useful, but WITHOUT ANY WARRANTY; without even the
