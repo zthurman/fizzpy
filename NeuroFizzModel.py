a0153bdcfda1477360cf1c03d582d821e56f3664
wonkybadonk <zrt81491@gmail.com>
2015-10-03 12:54:25 -0700

    condensed models file considerably

--- NeuroFizzModel.py
+++ NeuroFizzModel.py
@@ -18,14 +18,14 @@ class System():
 
     # van der Pol oscillator
 
-    def VDP(System, name="van der Pol oscillator", x0 = np.array([0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
+    def VDP(System, name = 'van der Pol oscillator', x0 = np.array([0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
         def model(self,x,t, mu = 1):
             return np.array([x[1]/mu,
                              (-x[0] + x[1]*(1-x[0]**2))*mu])
 
     # EPSP - excitatory post-synaptic potential
 
-    def EPSP(System):
+"""    def EPSP(System):
         name = "EPSP"
         x0 = np.array([0,0,0])
 
@@ -54,28 +54,25 @@ class System():
                 g_syn[int(i+1)] = g_syn[int((i))]-np.dot((delta_t/tau_syn), g_syn[int((i))])
                 I_syn[int(i+1)] = np.dot(g_syn[int(i+1)], v_m[int((i))]-E_syn)
                 v_m[int(i+1)] = v_m[int((i))]-np.dot(np.dot((delta_t/c_m),g_L), v_m[int((i))]) \
-                                -np.dot((delta_t/c_m), I_syn[int(i+1)])
+                                -np.dot((delta_t/c_m), I_syn[int(i+1)])"""
 
     # FN neuron model
 
-    def FN(System, name = "Fitzhugh-Nagumo", x0 = np.array([0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
+    def FN(System, name = 'Fitzhugh-Nagumo', x0 = np.array([0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
         def model(self,x,t, a = 0.75, b = 0.8, c = 3,  i = -0.40):
             return np.array([c*(x[0]+ x[1]- x[0]**3/3 + i),
                              -1/c*(x[0]- a + b*x[1])])
 
     # ML neuron model
 
-    def ML(System, name = "Morris-Lecar", x0 = np.array([0,0]), t_array = (0, 1000, 0.30)):
+    def ML(System, name = 'Morris-Lecar', x0 = np.array([0,0]), t_array = (0, 1000, 0.30)):
         def model(self,x,t,c = 20,vk=-84,gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,i = 80):
             return np.array([(-gca*(0.5*(1 + mt.tanh((x[0] - v1)/v2)))*(x[0]-vca) - gk*x[1]*(x[0]-vk) - gl*(x[0]-vl) + i),
                             (phi*((0.5*(1 + mt.tanh((x[0] - v3)/v4))) - x[1]))/(1/mt.cosh((x[0] - v3)/(2*v4)))])
 
     # IZ neuron model
 
-    def IZ(System, name = "Izhikevich", x0 = np.array([0,0]), t_array = ):
-        name = "Izhikevich"
-        x0 = np.array([0,0])
-
+    def IZ(System, name = 'Izhikevich', x0 = np.array([0,0]), t_array = np.arange(0, 300, 0.1)):
         def model(self,x,t, a = 0.02, b = 0.2, c = -55, d = 2, i = 10):
             if x[0] >= 30:
                 x[0] = c
@@ -85,21 +82,15 @@ class System():
 
     # HR neuron model
 
-    def HR(System):
-        name = "Hindmarsh-Rose"
-        x0 = np.array([3, 0, -1.2])
-
+    def HR(System, name = 'Hindmarsh-Rose', x0 = np.array([3, 0, -1.2]), t_array = np.arange(0, 800, 0.1)):
         def model(self,x,t, a = 1.0, b = 3.0, c = 1.0, d = 5.0, r = 0.006, s = 4.0, I = 1, xnot = -1.5):
             return np.array([x[1] - a*(x[0]**3) + (b*(x[0]**2)) - x[2] + I,
                             c - d*(x[0]**2) - x[1],
                             r*(s*(x[0] - xnot) - x[2])])
 
-    # HH neuron model
+    # Hodgkins-Huxley neuron model
 
-    def HH(System):
-        name = "Hodgkins-Huxley"
-        x0 = np.array([0.01,0.01,0.01,0.01])
-
+    def HH(System, name = 'Hodgkins-Huxley', x0 = np.array([0.01,0.01,0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
         def model(self,x,t, g_K=36, g_Na=120, g_L=0.3, E_K=12, E_Na=-115, E_L=-10.613, C_m=1, I=-10):
             alpha_n = (0.01*(x[0]+10))/(exp((x[0]+10)/10)-1)
             beta_n = 0.125*exp(x[0]/80)
@@ -112,12 +103,9 @@ class System():
                             alpha_m*(1-x[2]) - beta_m*x[2],
                             alpha_h*(1-x[3]) - beta_h*x[3]])
 
-    # RD model for geomagnetic reversal
+    # Rikitake dynamo model for geomagnetic reversal
 
-    def RD(System):
-        name = 'Rikitake Dynamo'
-        x0 = np.array([-1.4, -1, -1, -1.4, 2.2, -1.5])
-
+    def RD(System, name = 'Rikitake Dynamo', x0 = np.array([-1.4, -1, -1, -1.4, 2.2, -1.5]), t_array = np.arange(0, 100, 0.01)):
         def model(self, x,t, m = 0.5, g = 50, r = 8, f = 0.5):
             return np.array([r*(x[3] - x[0]),
                              r*(x[2] - x[1]),
@@ -126,16 +114,16 @@ class System():
                              g*(1 - (1 + m)*x[0]*x[2] + m*x[0]*x[1]) - f*x[4],
                              g*(1 - (1 + m)*x[1]*x[3] + m*x[1]*x[0]) - f*x[5]])
 
-    # W neuron model
+    # Hugh Wilson neuron model
 
-    def W(System):
+"""    def W(System):
         name = 'Wilson Model'
         x0 = []
 
         def model(self, x, t):
             pass
 
-        """def Wilson(x,t, g_K=26, g_T=0.1, g_H=5, E_K=-0.95, E_Na=0.50, E_Ca=1.20, C=0.01, E_H=-0.95, tau_T=14, tau_R=4.2, tau_H=45, I=1):
+        def Wilson(x,t, g_K=26, g_T=0.1, g_H=5, E_K=-0.95, E_Na=0.50, E_Ca=1.20, C=0.01, E_H=-0.95, tau_T=14, tau_R=4.2, tau_H=45, I=1):
                 G_K = g_K*x[1]*100
                 G_Na = 17.8 + 47.6*x[0] + 33.8*(x[0]**2)
                 G_Ca = g_T*x[2]*100
@@ -147,38 +135,18 @@ class System():
                                   -(x[2] - Tnot)*(1/tau_T),
                                   -(x[3] - 3*x[2])*(1/tau_H)])"""
 
-    # L atmospheric model
+    # Lorenz atmospheric model, strange attractor
 
-    def L(System):
-        name = "Lorenz Equations"
-        x0 = np.array([1.0, 2.0, 1.0])
-
+    def L(System, name = 'Lorenz Equations', x0 = np.array([-1.4, -1, -1, -1.4, 2.2, -1.5]), t_array = np.arange(0, 100, 0.01)):
         def model(self, x, t, sigma = 10.0, rho = 28.0, beta = 10.0/3):
             return np.array([sigma * (x[1] - x[0]),
                              rho*x[0] - x[1] - x[0]*x[2],
                              x[0]*x[1] - beta*x[2]])
 
-    # R geomagnetic polarity reversal model
+    # Robbins geomagnetic polarity reversal model
 
-    def R(System):
-        name = "Robbins Equations"
-        x0 = np.array([0.00032,0.23,0.51])
-
+    def R(System, name = 'Robbins Equations', x0 = np.array([0.00032,0.23,0.51]), t_array = np.arange(0, 200, 0.1)):
         def model(self,x,t, V = 1, sigma = 5, R = 13):
             return np.array([R - x[1]*x[2] - V*x[0],
                              x[0]*x[2] - x[1],
                              sigma*(x[1] - x[2])])
-
-# van der Pol oscillator
-
-class VDP(System, name="van der Pol oscillator", x0 = np.array([0.01,0.01]), t_array = np.arange(0, 100, 0.02)):
-    def model(self,x,t, mu = 1):
-        return np.array([x[1]/mu,
-                         (-x[0] + x[1]*(1-x[0]**2))*mu])
-
-# ML neuron model
-
-class ML(System, name = "Morris-Lecar", x0 = np.array([0,0]), t_array =(0, 1000, 0.30)):
-    def model(self,x,t,c = 20,vk=-84,gk = 8,vca = 130,gca = 4.4,vl = -60,gl = 2,phi = 0.04,v1 = -1.2,v2 = 18,v3 = 2,v4 = 30,i = 80):
-        return np.array([(-gca*(0.5*(1 + mt.tanh((x[0] - v1)/v2)))*(x[0]-vca) - gk*x[1]*(x[0]-vk) - gl*(x[0]-vl) + i),
-                        (phi*((0.5*(1 + mt.tanh((x[0] - v3)/v4))) - x[1]))/(1/mt.cosh((x[0] - v3)/(2*v4)))])
