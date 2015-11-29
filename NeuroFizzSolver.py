4685d0c6ff6a979fbf1c67f8b2d51230309fd52e
zthurman <zrt81491@gmail.com>
2015-11-29 14:42:06 -0800

    solvers are now squeaky like Mr. Clean

--- NeuroFizzSolver.py
+++ NeuroFizzSolver.py
@@ -5,9 +5,7 @@
 
 from __future__ import division
 from NeuroFizzModel import VDP, FN, ML, IZ, HR, HH
-from scipy import *
 import numpy as np
-import math as mt
 
 # global solver class
 
@@ -86,7 +84,7 @@ class euler(Solver):
 
     def evaluate(self):
         for i in range(0, self.Nsize-1):
-            k1 = self.eqns(self.X[i], self.tsp[i])
+            k1 = self.model(self.X[i], self.tsp[i])
             self.X[i+1] = self.X[i] + k1*self.dt
         return self.X
 
@@ -105,20 +103,32 @@ class ord2(Solver):
 
     def evaluate(self):
         for i in range(0, self.Nsize-1):
-            k1 = self.eqns(self.X[i], self.tsp[i])
-            k2 = self.eqns(self.X[i], self.tsp[i]) + k1*(self.dt/2)
+            k1 = self.model(self.X[i], self.tsp[i])
+            k2 = self.model(self.X[i], self.tsp[i]) + k1*(self.dt/2)
             self.X[i+1] = self.X[i] + k2*self.dt
         return self.X
 
+# fourth order Runge-Kutte solver
 
-test = VDP()
+class rk4(Solver):
 
-soln = ord2(test.name, test.x0, test.dt, test.t_array, test.eqns)
+    def __init__(self, name, x0, dt, t_array, eqns):
+        self.modelname = name
+        self.dt = dt
+        self.tsp = t_array
+        self.Nsize = np.size(self.tsp)
+        self.X = np.empty((self.Nsize, np.size(x0)))
+        self.X[0] = x0
+        self.model = eqns
 
-print(soln.X[:,0])
-print(soln.tsp)
-print(soln.modelname)
-
+    def evaluate(self):
+        for i in range(0, self.Nsize-1):
+            k1 = self.model(self.X[i], self.tsp[i])
+            k2 = self.model(self.X[i] + self.dt/2*k1, self.tsp[i] + self.dt/2)
+            k3 = self.model(self.X[i] + self.dt/2*k2, self.tsp[i] + self.dt/2)
+            k4 = self.model(self.X[i] + self.dt*k3, self.tsp[i] + self.dt)
+            self.X[i+1] = self.X[i] + self.dt/6*(k1 + 2*k2 + 2*k3 + k4)
+        return self.X
 
 # Unit Test
 
@@ -127,8 +137,9 @@ def SolverTest():
         test = i
         print(test.name, test.x0, test.t0, test.t1, test.dt, test.t_array, test.eqns)
         for j in [euler(test.name, test.x0, test.dt, test.t_array, test.eqns),
-                  ord2(test.name, test.x0, test.dt, test.t_array, test.eqns)]:
+                  ord2(test.name, test.x0, test.dt, test.t_array, test.eqns),
+                  rk4(test.name, test.x0, test.dt, test.t_array, test.eqns)]:
             soln = j
-            print(soln.tsp, soln.Nsize, soln.X, soln.X[0], soln.X[:,0])
+            print(soln.tsp, soln.Nsize, soln.X, soln.X[0], soln.X[:,0], soln.evaluate())
 
 solvertester = SolverTest()
