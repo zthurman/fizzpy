57f56b5ee3f181f3f3ce919e286e952d17649f5b
zthurman <zrt81491@gmail.com>
2015-11-29 17:09:49 -0800

    added basic plotter and trying to get rid of the egg errawr so that I can get matplotlib going

---
+++ NeuroFizzPlot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# NeuroFizzMath - NeuroFizzPlot
+# Copyright (C) 2015 Zechariah Thurman
+# GNU GPLv2
+
+from __future__ import division
+from NeuroFizzModel import VDP, FN, ML, IZ, HR, HH
+from NeuroFizzSolver import euler, ord2, rk4
