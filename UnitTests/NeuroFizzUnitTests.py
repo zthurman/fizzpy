23cfb7b16a63a763811c46bc091f3a419617ed66
zthurman <zrt81491@gmail.com>
2015-11-29 20:39:59 -0800

    unit test galore!

--- UnitTests/NeuroFizzUnitTests.py
+++ UnitTests/NeuroFizzUnitTests.py
@@ -13,24 +13,28 @@ import time as tm
 def ModelTest():
     for i in [VDP(), FN(), ML(), IZ(), HR(), HH()]:
         test = i
-        print(test.name, test.x0, test.t0, test.t1, test.dt, test.t_array, test.eqns)
+        print(test.name, test.xaxis, test.yaxis, test.x0, test.t0, test.t1, test.dt, test.t_array, test.eqns)
 
 starttime = tm.time()
 modeltester = ModelTest()
-print(" %s seconds" % (tm.time() - starttime))
+elapsedtime = (tm.time() - starttime)
+print(" %s seconds" % elapsedtime)
 
 # Solver Test
 
 def SolverTest():
     for i in [VDP(), FN(), ML(), IZ(), HR(), HH()]:
         test = i
-        print(test.name, test.x0, test.t0, test.t1, test.dt, test.t_array, test.eqns)
+        print(test.name, test.xaxis, test.yaxis, test.x0, test.t0, test.t1, test.dt, test.t_array, test.eqns)
         for j in [euler(test.name, test.xaxis, test.yaxis, test.x0, test.dt, test.t_array, test.eqns),
                   ord2(test.name, test.xaxis, test.yaxis, test.x0, test.dt, test.t_array, test.eqns),
                   rk4(test.name, test.xaxis, test.yaxis, test.x0, test.dt, test.t_array, test.eqns)]:
             soln = j
             print(soln.tsp, soln.Nsize, soln.X, soln.X[0], soln.X[:,0], soln.evaluate())
 
-starttime = tm.time()
+starttime1 = tm.time()
 solvertester = SolverTest()
-print(" %s seconds" % (tm.time() - starttime))
+elapsedtime1 = (tm.time() - starttime1)
+
+
+print("ModelTest took {0} seconds to execute while SolverTest took {1} seconds to execute" .format(elapsedtime, elapsedtime1))
