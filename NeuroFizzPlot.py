d085afd29d5f090c670266898782a4a0ecb9b89b
zthurman <zrt81491@gmail.com>
2015-12-05 10:00:58 -0800

    moved timer to unittests directory and removed extraneous thingies from plotterface

--- NeuroFizzPlot.py
+++ NeuroFizzPlot.py
@@ -7,67 +7,11 @@ from __future__ import division
 from NeuroFizzModel import VDP, FN, ML, IZ, HR, HH
 from NeuroFizzSolver import euler, ord2, rk4
 import matplotlib.pyplot as plt
-import time as tm
 
-# Time execution of the VDP evaluate with different solvers
-
-# Euler Solver
-
-starttime = tm.time()
-eqns = VDP()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for VDP" % (tm.time() - starttime))
-
-starttime = tm.time()
-eqns = FN()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for FN" % (tm.time() - starttime))
-
-starttime = tm.time()
-eqns = ML()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for ML" % (tm.time() - starttime))
-
-starttime = tm.time()
-eqns = IZ()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for IZ" % (tm.time() - starttime))
-
-starttime = tm.time()
-eqns = HR()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for HR" % (tm.time() - starttime))
-
-starttime = tm.time()
-eqns = HH()
-solvedmodel = euler(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds for HH" % (tm.time() - starttime))
-
-# Second Order Runge-Kutte Solver
-
-starttime = tm.time()
-solvedmodel = ord2(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds" % (tm.time() - starttime))
-
-# Fourth Order Runge-Kutte
-
-starttime = tm.time()
-solvedmodel = rk4(eqns.name, eqns.xaxis, eqns.yaxis, eqns.x0, eqns.dt, eqns.t_array, eqns.eqns)
-membranepotential = solvedmodel.evaluate()
-print(" %s seconds" % (tm.time() - starttime))
-
-
-# Do a test plot and save it as a png in the working directory
+# Do some test plots and save outputs as a png in the working directory
 
 def do_tplot():
-    neuron = HH()
+    neuron = VDP()
     solvedneuron = ord2(neuron.name, neuron.xaxis, neuron.yaxis, neuron.x0, neuron.dt, neuron.t_array, neuron.eqns)
     membranepotential = solvedneuron.evaluate()
     plt.figure()
