96a38b29bc891c19315587c8e2cd562f01e6c1e5
zrt81491 <zrt81491@gmail.com>
2016-12-15 20:38:21 -0800

    modelSelector, solverSelector and solutionGenerator are all grossly verbose but they all function. Elegantification comes next.

--- NeuroFizzMath.py
+++ NeuroFizzMath.py
@@ -5,7 +5,7 @@
 
 from __future__ import division
 import numpy as np
-# import math as mt
+import math as mt
 
 
 # class Neuron:
@@ -83,9 +83,53 @@ def RungeKutte4(t0=0, x0=np.array([1]), t1=5, dt=0.01, model=None):
     return X
 
 
-def VanDerPol(x,t):
+def VanDerPol(x, t):
     return np.array([x[1],
                     -x[0] + x[1]*(1-x[0]**2)])
+
+
+def LeakyIntegrateandFire(x, t, u_th=-55, u_reset=-75, u_eq=-65, r=10, i=1.2):
+    if x[0] >= u_th:
+        x[0] = u_reset
+    return np.array([-(x[0] - u_eq) + r*i])
+
+
+def FitzhughNagumo(x, t, a=0.75, b=0.8, c=3, i=-0.40):
+    return np.array([c*(x[0] + x[1] - x[0]**3/3 + i),
+                    -1/c*(x[0] - a + b*x[1])])
+
+
+def MorrisLecar(x, t, c=20, vk=-84, gk=8, vca=130, gca=4.4, vl=-60, gl=2, phi=0.04, v1=-1.2, v2=18, v3=2, v4=30,
+                iapp=80):
+    return np.array([(-gca*(0.5*(1 + mt.tanh((x[0] - v1)/v2)))*(x[0]-vca) - gk*x[1]*(x[0]-vk) - gl*(x[0]-vl) + iapp),
+                    (phi*((0.5*(1 + mt.tanh((x[0] - v3)/v4))) - x[1]))/(1/mt.cosh((x[0] - v3)/(2*v4)))])
+
+
+def Izhikevich(x, t, a=0.02, b=0.2, c=-65, d=2, i=10):
+    if x[0] >= 30:
+        x[0] = c
+        x[1] = x[1] + d
+    return np.array([0.04*(x[0]**2) + 5*x[0] + 140 - x[1] + i,
+                    a*(b*x[0] - x[1])])
+
+
+def HindmarshRose(x, t, a=1.0, b=3.0, c=1.0, d=5.0, r=0.006, s=4.0, i=1.3, xnot=-1.5):
+    return np.array([x[1] - a*(x[0]**3) + (b*(x[0]**2)) - x[2] + i,
+                    c - d*(x[0]**2) - x[1],
+                    r*(s*(x[0] - xnot) - x[2])])
+
+
+def HodgkinHuxley(x, t, g_K=36, g_Na=120, g_L=0.3, E_K=12, E_Na=-115, E_L=-10.613, C_m=1, I=-10):
+    alpha_n = (0.01*(x[0]+10))/(mt.exp((x[0]+10)/10)-1)
+    beta_n = 0.125*mt.exp(x[0]/80)
+    alpha_m = (0.1*(x[0]+25))/(mt.exp((x[0]+25)/10)-1)
+    beta_m = 4*mt.exp(x[0]/18)
+    alpha_h = (0.07*mt.exp(x[0]/20))
+    beta_h = 1 / (mt.exp((x[0]+30)/10)+1)
+    return np.array([(g_K*(x[1]**4)*(x[0]-E_K) + g_Na*(x[2]**3)*x[3]*(x[0]-E_Na) + g_L*(x[0]-E_L) - I)*(-1/C_m),
+                    alpha_n*(1-x[1]) - beta_n*x[1],
+                    alpha_m*(1-x[2]) - beta_m*x[2],
+                    alpha_h*(1-x[3]) - beta_h*x[3]])
 
 
 def solutionGenerator(modelname, solvername):
@@ -100,19 +144,69 @@ def solutionGenerator(modelname, solvername):
             elif solverSelector(solvername) == 3 and modelSelector(modelname) == 1:
                 solution = RungeKutte4(x0=np.array([0.01, 0.01]), t1=100, dt=0.01, model=VanDerPol)
                 return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 2:
+                solution = Euler(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=LeakyIntegrateandFire)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 2:
+                solution = SecondOrder(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=LeakyIntegrateandFire)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 2:
+                solution = RungeKutte4(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=LeakyIntegrateandFire)
+                return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 3:
+                solution = Euler(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=FitzhughNagumo)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 3:
+                solution = SecondOrder(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=FitzhughNagumo)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 3:
+                solution = RungeKutte4(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=FitzhughNagumo)
+                return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 4:
+                solution = Euler(x0=np.array([0, 0]), t1=100, dt=0.02, model=MorrisLecar)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 4:
+                solution = SecondOrder(x0=np.array([0, 0]), t1=100, dt=0.02, model=MorrisLecar)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 4:
+                solution = RungeKutte4(x0=np.array([0, 0]), t1=100, dt=0.02, model=MorrisLecar)
+                return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 5:
+                solution = Euler(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=Izhikevich)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 5:
+                solution = SecondOrder(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=Izhikevich)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 5:
+                solution = RungeKutte4(x0=np.array([0.01, 0.01]), t1=100, dt=0.02, model=Izhikevich)
+                return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 6:
+                solution = Euler(x0=np.array([0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HindmarshRose)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 6:
+                solution = SecondOrder(x0=np.array([0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HindmarshRose)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 6:
+                solution = RungeKutte4(x0=np.array([0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HindmarshRose)
+                return solution
+            elif solverSelector(solvername) == 1 and modelSelector(modelname) == 7:
+                solution = Euler(x0=np.array([0.01, 0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HodgkinHuxley)
+                return solution
+            elif solverSelector(solvername) == 2 and modelSelector(modelname) == 7:
+                solution = SecondOrder(x0=np.array([0.01, 0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HodgkinHuxley)
+                return solution
+            elif solverSelector(solvername) == 3 and modelSelector(modelname) == 7:
+                solution = RungeKutte4(x0=np.array([0.01, 0.01, 0.01, 0.01]), t1=100, dt=0.02, model=HodgkinHuxley)
+                return solution
             else:
                 solution = "Cheese please!"
                 return solution
 
 
-x = modelSelector('HH')
-
-print(x)
-
-y = solverSelector('rk4')
-
-print(y)
-
-z = solutionGenerator('VDP', 'rk4')
-
-print(z)
+if __name__ == '__main__':
+    x = modelSelector('ML')
+    print(x)
+    y = solverSelector('rk4')
+    print(y)
+    z = solutionGenerator('HH', 'rk4')
+    print(z)
