341c778fb7a03eb773f4ee0a84cd90be3eebe78b
zrt81491 <zrt81491@gmail.com>
2016-12-17 19:03:45 -0800

    foobar challenge

--- Python/cheese.py
+++ Python/cheese.py
@@ -1,10 +1,10 @@
 def answer(s):
-    s = "Abc'S"
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o,'
                 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     upperalphabet = [x.upper() for x in alphabet]
     reversedalphabet = alphabet[::-1]
     symbol = "~`!@#$%^&*()_-+={}[]:>;',</?*-+"
+    space = " "
     reversed_s = ""
     for i in s:
         case = i.islower()
@@ -18,8 +18,10 @@ def answer(s):
                 reversed_s += i
             elif i in symbol:
                 reversed_s += i
+            elif i == space:
+                reversed_s += i
     return reversed_s
 
 if __name__ == '__main__':
-    string = "Abc'S"
+    string = "Abc'S has cheese"
     print(answer(string))
