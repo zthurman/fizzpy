d0f512de5fdc9ca7ce9d83f595f0c44e253b0582
zrt81491 <zrt81491@gmail.com>
2016-12-17 19:07:54 -0800

    foobar challenge, megaderped

--- Python/cheese.py
+++ Python/cheese.py
@@ -1,17 +1,17 @@
 def answer(s):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o,'
                 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    upperalphabet = [x.upper() for x in alphabet]
     reversedalphabet = alphabet[::-1]
+    upperalphabet = [x.upper() for x in reversedalphabet]
     symbol = "~`!@#$%^&*()_-+={}[]:>;',</?*-+"
     space = " "
     reversed_s = ""
     for i in s:
         case = i.islower()
         if case:
-            if i in alphabet:
-                index = alphabet.index(i)
-                newletter = reversedalphabet[index]
+            if i in reversedalphabet:
+                index = reversedalphabet.index(i)
+                newletter = alphabet[index]
                 reversed_s += newletter
         elif not case:
             if i in upperalphabet:
@@ -23,5 +23,5 @@ def answer(s):
     return reversed_s
 
 if __name__ == '__main__':
-    string = "Abc'S has cheese"
+    string = "Zyx uid szh"
     print(answer(string))
