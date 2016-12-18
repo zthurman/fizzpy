cd16d8d227f9eb1b847a2aa8e616a66e91cef5dd
zrt81491 <zrt81491@gmail.com>
2016-12-17 19:31:50 -0800

    derpsause extraordinaire

--- Python/cheese.py
+++ Python/cheese.py
@@ -20,10 +20,43 @@ def answer(s):
                 reversed_s += i
             elif i == space:
                 reversed_s += i
+            elif "\\" in i:
+                reversed_s += i
+    return reversed_s
+
+
+def stringjumbler(s):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    upperalphabet = [x.upper() for x in alphabet]
+    reversedalphabet = alphabet[::-1]
+    symbol = "~`!@#$%^&*()_-+={}[]:>;',</?*-+"
+    space = " "
+    reversed_s = ""
+    for i in s:
+        case = i.islower()
+        if case:
+            if i in reversedalphabet:
+                index = alphabet.index(i)
+                newletter = reversedalphabet[index]
+                reversed_s += newletter
+        elif not case:
+            if i in upperalphabet:
+                reversed_s += i
+            elif i in symbol:
+                reversed_s += i
+            elif i == space:
+                reversed_s += i
+            elif "\\" in i:
+                reversed_s += i
     return reversed_s
 
 if __name__ == '__main__':
-    string = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
-    print(answer(string))
+    # string = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
+    string = "Eer\\mahgerdwat in dE Fa*&%[]i ng fre~"
+    print(string)
+    reversedstring = stringjumbler(string)
+    print(reversedstring)
+    print(answer(reversedstring))
 
 
