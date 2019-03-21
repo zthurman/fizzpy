139c67f21a622117c2deb1d3216817211ded5036
Wonkybadonk <zrt81491@gmail.com>
2019-03-20 19:51:00 -0700

    This should address significant roadblocks preventing the close of #2 and #5.

--- examples/Helper.py
+++ examples/Helper.py
@@ -100,3 +100,98 @@ def plotface(arg1, arg2=None, xlim=None, ylim=None, xlabel=None, ylabel=None, gr
     if grid is not None:
         plt.grid()
     return plt.show()
+
+
+class Model:
+
+    """ Model class
+    Used for storing methods that generalize to all models.
+    """
+
+    def __init__(
+            self,
+            initial_conditions=None,
+            model_parameters=None,
+            final_time=None,
+            time_steps=None):
+        self.initial_conditions = initial_conditions
+        self.model_parameters = model_parameters
+        self.final_time = final_time
+        self.time_steps = time_steps
+
+    def init_converter(self, arg1: np.array) -> tf.constant:
+        """Initial conditions converter.
+
+        Converts the initial_conditions ndarray into a Tensorflow
+        constant n-dimensional tensor.
+
+        `tf.constant <https://www.tensorflow.org/api_docs/python/tf/constant>`_
+
+        Parameters
+        ----------
+        arg1
+            Initial conditions for the system of ODEs to be solved.
+
+        Returns
+        -------
+        tf.constant
+            Constant tf.float64 n-d tensor based on initial_conditions
+            provided.
+
+        """
+        init_state = tf.constant(arg1, dtype=tf.float64)
+        return init_state
+
+    def ode_solver(self, arg1: tf.stack, arg2: np.ndarray) -> list:
+        """Ordinary Differential Equation (ODE) solver.
+
+        Uses Tensorflow/Numpy odeint to numerically solve the input system
+        of ODEs given the provided initial conditions and optional time
+        array parameters.
+
+        `odeint <https://www.tensorflow.org/api_docs/python/tf/contrib/integrate/odeint>`_
+
+        Parameters
+        ----------
+        arg1
+            Tensorflow stack representing the equations for the system of ODEs.
+        arg2
+            Initial conditions for the system of ODEs to be solved.
+
+        Returns
+        -------
+        list
+            y: (n+1)-d tensor. Contains the solved value of y for each desired
+            time point in t.
+            info_dict: only if full_output=True for odeint, additional info.
+
+        """
+        t = np.linspace(0, self.final_time, num=self.time_steps)
+        tensor_state, tensor_info = tf.contrib.integrate.odeint(arg1, self.init_converter(arg2), t, full_output=True)
+        return [tensor_state, tensor_info]
+
+    def tf_session(self, arg1: tf.stack, arg2: np.ndarray) -> np.ndarray:
+        """Tensorflow session runner.
+
+        Uses a Tensorflow session run to evaluate the provided system of ODEs.
+
+        `tf.Session.run <https://www.tensorflow.org/api_docs/python/tf/Session#run>`_
+
+        Parameters
+        ----------
+        arg1
+            Tensorflow stack representing the equations for the system of ODEs.
+        arg2
+            Initial conditions for the system of ODEs to be solved.
+
+        Returns
+        -------
+        np.ndarray
+            Returns the transpose of the (n+1)-d state tensor returned from
+            ode_solver after it's been solved in the Tensorflow session.
+
+        """
+        sess = tf.Session()
+        state, info = sess.run(self.ode_solver(arg1, arg2))
+        output = state.T
+        return output
